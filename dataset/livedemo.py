98f2b9a2e32a6493cbb90bc377ba08d7150edb40
Morteza Golzan <42976778+mortezagolzan@users.noreply.github.com>
2022-01-25 22:48:36 +0330

    Add files via upload

---
+++ dataset/livedemo.py
@@ -0,0 +1,52 @@
+import os
+import cv2
+import paddle.fluid as fluid
+import numpy as np
+import pickle
+import random
+from .datasetbase import DatasetBase
+from facenet_pytorch import MTCNN
+from PIL import Image
+
+
+class LIVEDEMO(DatasetBase):
+    def __init__(self,
+                 img_prefix,
+                 ann_file,
+                 img_scale,
+                 img_norm_cfg,
+                 extra_aug=None):
+
+        super(LIVEDEMO, self).__init__(
+            img_prefix,
+            ann_file,
+            img_scale,
+            img_norm_cfg,
+            extra_aug)
+
+    def get_face(self,img):
+        mtcnn = MTCNN(image_size=224,post_process=False, margin=0)
+        # frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        frame = Image.fromarray(img)
+
+        face = mtcnn(frame)
+        face = face.permute(1, 2, 0).int().numpy()
+
+        return face
+
+    def test(self):
+        def reader():
+            cap= cv2.VideoCapture('/content/rooz_roozphone_fake.avi')
+            p=0
+            while (p<1):
+              p=p+1
+              ret, frame = cap.read()
+              if ret:
+                try:
+                  face = self.get_face(frame)
+                except:
+                  continue
+                img = self.img_transform(face, self.img_scale)
+                label = 1
+                yield img, label
+        return reader
